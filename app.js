9609365a4599eb14ad5c77a3865682807a6d0e45
janidu98 <janinduchathushan@gmail.com>
2023-07-27 21:03:30 +0530

    C1

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+const adminRouter = require('./routes/admin');
+const shopRouter = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use(adminRouter);
+app.use(shopRouter);
+
+app.listen(3000);
